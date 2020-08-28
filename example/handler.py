77393dc05f3e88e81605ef4394b91ae5091baef4
Wei Wu <weiwu995@gmail.com>
2020-08-28 10:29:08 -0500

    Tutorial

--- example/handler.py
+++ example/handler.py
@@ -6,7 +6,7 @@ def handle(data, client):
     print("Did sleep. Will sleep some more, but first, print another line")
     print("Here it is.")
     time.sleep(1.0)
-    print("Ok, I'm done now. Fetching an asset")
+    print("Ok, I'm done now. Fetching an asset!!!!")
     asset = client.assets.retrieve(id = 8091887440135311)
     return {
         "result": 3.0 * data["value"],
